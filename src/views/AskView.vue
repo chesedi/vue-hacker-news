538f573b4dfa7fb7b31dc2e317a9cd5a527a4aa3
DESKTOP-OKR5BMP\etoos <a02t02@gmail.com>
2021-06-14 18:01:53 +0900

    first commit

---
+++ src/views/AskView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-for="item in ask">{{ item }}</div>
+  </div>
+</template>
+
+<script>
+import { fetchAskList } from '../api/index.js';
+export default {
+  data() {
+    return {
+      ask: []
+    }
+  },
+  created() {
+    fetchAskList()
+      .then(response => this.users = response.data)
+      .catch(function(error) {
+        console.error(error);
+      });
+  },
+}
+</script>
+
+<style>
+
+</style>
